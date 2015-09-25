d232a8aa50ba1022253f6f24691b46a46d84df25
Charles Madere <charlesrmadere@gmail.com>
2015-09-24 23:08:33 -0700

    added new StatusBarView class

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/views/StatusBarView.java
@@ -0,0 +1,40 @@
+package com.garpr.android.views;
+
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.garpr.android.R;
+
+
+public class StatusBarView extends View {
+
+
+    public StatusBarView(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    public StatusBarView(final Context context, final AttributeSet attrs, final int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public StatusBarView(final Context context, final AttributeSet attrs, final int defStyleAttr,
+            final int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+    }
+
+
+}
