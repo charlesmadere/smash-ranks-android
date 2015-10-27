ad864314d08c9abb97e626d24fa57bae5a07d1bf
Charles Madere <charlesrmadere@gmail.com>
2015-10-26 21:56:49 -0700

    when a player has been added to favorites, the icon becomes disabled

--- smash-ranks-android/app/src/main/java/com/garpr/android/activities/PlayerActivity.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/activities/PlayerActivity.java
@@ -257,7 +257,7 @@ public class PlayerActivity extends BaseToolbarListActivity implements
         switch (item.getItemId()) {
             case R.id.activity_player_menu_favorite:
                 FavoritesStore.add(mPlayer);
-                mFavorite.setVisible(false);
+                mFavorite.setEnabled(false);
                 break;
 
             case R.id.activity_player_menu_share:
