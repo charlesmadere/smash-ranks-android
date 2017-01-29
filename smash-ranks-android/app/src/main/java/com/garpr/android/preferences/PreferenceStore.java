275cb4308d78cd829b68c79b5c0e63d40bcaf7a1
Charles Madere <charlesrmadere@gmail.com>
2017-01-29 01:25:06 -0800

    progress

--- smash-ranks-android/app/src/main/java/com/garpr/android/preferences/PreferenceStore.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/preferences/PreferenceStore.java
@@ -1,12 +1,7 @@
 package com.garpr.android.preferences;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-
 public interface PreferenceStore {
 
     void clearAll();
-    Context getContext();
-    SharedPreferences getDefaultSharedPreferences();
 
 }
--- smash-ranks-android/app/src/main/java/com/garpr/android/preferences/PreferenceStoreImpl.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/preferences/PreferenceStoreImpl.java
@@ -1,8 +1,6 @@
 package com.garpr.android.preferences;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 
 public class PreferenceStoreImpl implements PreferenceStore {
 
@@ -13,23 +11,9 @@ public class PreferenceStoreImpl implements PreferenceStore {
         mContext = context.getApplicationContext();
     }
 
-    private void clear(final SharedPreferences sharedPreferences) {
-        sharedPreferences.edit().clear().apply();
-    }
-
     @Override
     public void clearAll() {
-        clear(getDefaultSharedPreferences());
-    }
-
-    @Override
-    public Context getContext() {
-        return mContext;
-    }
-
-    @Override
-    public SharedPreferences getDefaultSharedPreferences() {
-        return PreferenceManager.getDefaultSharedPreferences(mContext);
+        // TODO
     }
 
 }
