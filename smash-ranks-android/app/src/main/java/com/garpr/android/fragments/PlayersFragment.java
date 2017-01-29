4dbf407c4476c56ae79b2cdcd359412d66274b55
Charles Madere <charlesrmadere@gmail.com>
2017-01-28 22:24:17 -0800

    added layout for players fragment

--- smash-ranks-android/app/src/main/java/com/garpr/android/fragments/PlayersFragment.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/fragments/PlayersFragment.java
@@ -1,13 +1,33 @@
 package com.garpr.android.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.garpr.android.R;
+
 public class PlayersFragment extends BaseFragment {
 
     private static final String TAG = "PlayersFragment";
 
 
+    public static PlayersFragment create() {
+        return new PlayersFragment();
+    }
+
     @Override
     protected String getFragmentName() {
         return TAG;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container,
+            @Nullable final Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_players, container, false);
     }
 
 }
