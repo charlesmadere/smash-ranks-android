9819b1690eedbc8178686f1f72e12f2f734ca3ce
Charles Madere <charlesrmadere@gmail.com>
2017-01-29 00:25:23 -0800

    added rankings list

--- smash-ranks-android/app/src/main/java/com/garpr/android/models/RankingsBundle.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/models/RankingsBundle.java
@@ -2,10 +2,17 @@ package com.garpr.android.models;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.support.annotation.Nullable;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+
 public class RankingsBundle implements Parcelable {
+
+    @Nullable
+    @SerializedName("ranking")
+    private ArrayList<Ranking> mRankings;
 
     @SerializedName("time")
     private SimpleDate mTime;
@@ -13,10 +20,18 @@ public class RankingsBundle implements Parcelable {
     @SerializedName("id")
     private String mId;
 
+    @SerializedName("region")
+    private String mRegion;
+
 
     @Override
     public boolean equals(final Object obj) {
         return obj instanceof RankingsBundle && mId.equals(((RankingsBundle) obj).mId);
+    }
+
+    @Nullable
+    public ArrayList<Ranking> getRankings() {
+        return mRankings;
     }
 
     public SimpleDate getTime() {
@@ -28,6 +43,10 @@ public class RankingsBundle implements Parcelable {
         return mId.hashCode();
     }
 
+    public boolean hasRankings() {
+        return mRankings != null && !mRankings.isEmpty();
+    }
+
     @Override
     public int describeContents() {
         return 0;
@@ -35,16 +54,20 @@ public class RankingsBundle implements Parcelable {
 
     @Override
     public void writeToParcel(final Parcel dest, final int flags) {
+        dest.writeTypedList(mRankings);
         dest.writeParcelable(mTime, flags);
         dest.writeString(mId);
+        dest.writeString(mRegion);
     }
 
     public static final Creator<RankingsBundle> CREATOR = new Creator<RankingsBundle>() {
         @Override
         public RankingsBundle createFromParcel(final Parcel source) {
             final RankingsBundle rb = new RankingsBundle();
+            rb.mRankings = source.createTypedArrayList(Ranking.CREATOR);
             rb.mTime = source.readParcelable(SimpleDate.class.getClassLoader());
             rb.mId = source.readString();
+            rb.mRegion = source.readString();
             return rb;
         }
 
