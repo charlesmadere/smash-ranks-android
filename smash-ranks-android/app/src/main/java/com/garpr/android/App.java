69ae348ec0b24d47e26b1804da764b20712e41f4
Charles Madere <charlesrmadere@gmail.com>
2017-01-29 01:59:08 -0800

    some cleanup

--- smash-ranks-android/app/src/main/java/com/garpr/android/App.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/App.java
@@ -1,24 +1,19 @@
 package com.garpr.android;
 
 import android.app.Application;
+
 import com.crashlytics.android.Crashlytics;
+
 import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
 
-    private static App sInstance;
-
-
-    public static App getInstance() {
-        return sInstance;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
-        sInstance = this;
-
         Fabric.with(this, new Crashlytics());
+
+        
     }
 
 }
