344f24740586089d92af2ac00a1d459f59cdd775
Charles Madere <charlesrmadere@gmail.com>
2017-01-29 00:20:31 -0800

    created new SimpleDate class

--- smash-ranks-android/app/src/main/java/com/garpr/android/models/RankingsBundle.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/models/RankingsBundle.java
@@ -3,7 +3,13 @@ package com.garpr.android.models;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.google.gson.annotations.SerializedName;
+
 public class RankingsBundle implements Parcelable {
+
+    @SerializedName("time")
+    private SimpleDate mTime;
+
 
     @Override
     public int describeContents() {
@@ -12,14 +18,14 @@ public class RankingsBundle implements Parcelable {
 
     @Override
     public void writeToParcel(final Parcel dest, final int flags) {
-
+        dest.writeParcelable(mTime, flags);
     }
 
     public static final Creator<RankingsBundle> CREATOR = new Creator<RankingsBundle>() {
         @Override
         public RankingsBundle createFromParcel(final Parcel source) {
             final RankingsBundle rb = new RankingsBundle();
-
+            rb.mTime = source.readParcelable(SimpleDate.class.getClassLoader());
             return rb;
         }
 
---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/models/SimpleDate.java
@@ -0,0 +1,97 @@
+package com.garpr.android.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.text.TextUtils;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class SimpleDate implements Parcelable {
+
+    private static final SimpleDateFormat[] FORMATS = {
+            new SimpleDateFormat("MM/dd/yy", Locale.US)
+    };
+
+    private final Date mDate;
+
+
+    private SimpleDate(final Date date) {
+        mDate = date;
+    }
+
+    private SimpleDate(final Parcel source) {
+        mDate = new Date(source.readLong());
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return mDate.equals(obj);
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    @Override
+    public int hashCode() {
+        return mDate.hashCode();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(final Parcel dest, final int flags) {
+        dest.writeLong(mDate.getTime());
+    }
+
+    public static final Creator<SimpleDate> CREATOR = new Creator<SimpleDate>() {
+        @Override
+        public SimpleDate createFromParcel(final Parcel source) {
+            return new SimpleDate(source);
+        }
+
+        @Override
+        public SimpleDate[] newArray(final int size) {
+            return new SimpleDate[size];
+        }
+    };
+
+    public static final JsonDeserializer<SimpleDate> JSON_DESERIALIZER = new JsonDeserializer<SimpleDate>() {
+        @Override
+        public SimpleDate deserialize(final JsonElement json, final Type typeOfT,
+                final JsonDeserializationContext context) throws JsonParseException {
+            if (json == null || json.isJsonNull()) {
+                return null;
+            }
+
+            final String jsonString = json.getAsString();
+
+            if (TextUtils.isEmpty(jsonString)) {
+                return null;
+            }
+
+            for (final SimpleDateFormat format : FORMATS) {
+                try {
+                    return new SimpleDate(format.parse(jsonString));
+                } catch (final ParseException e) {
+                    // this Exception can be safely ignored
+                }
+            }
+
+            throw new JsonParseException("unable to parse date: " + jsonString);
+        }
+    };
+
+}
