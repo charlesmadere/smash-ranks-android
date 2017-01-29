408d61eb9d892fdff9a97d650f63c15552d96520
Charles Madere <charlesrmadere@gmail.com>
2017-01-29 00:47:18 -0800

    added rankings method

--- smash-ranks-android/app/src/main/java/com/garpr/android/networking/ServerApi.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/networking/ServerApi.java
@@ -1,7 +1,11 @@
 package com.garpr.android.networking;
 
+import android.support.annotation.NonNull;
+
+import com.garpr.android.models.RankingsBundle;
+
 public interface ServerApi {
 
-
+    void getRankings(@NonNull final String region, final ApiCall<RankingsBundle> call);
 
 }
--- smash-ranks-android/app/src/main/java/com/garpr/android/networking/ServerApiImpl.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/networking/ServerApiImpl.java
@@ -1,7 +1,14 @@
 package com.garpr.android.networking;
 
+import android.support.annotation.NonNull;
+
+import com.garpr.android.models.RankingsBundle;
+
 public class ServerApiImpl implements ServerApi {
 
+    @Override
+    public void getRankings(@NonNull final String region, final ApiCall<RankingsBundle> call) {
 
+    }
 
 }
