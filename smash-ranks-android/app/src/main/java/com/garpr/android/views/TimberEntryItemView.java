869a1e6c36d514a2c7cd10d48248a1e6dc103c61
Charles Madere <charlesrmadere@gmail.com>
2017-02-02 19:05:54 -0800

    TimberEntryItemView now displays stuff

--- smash-ranks-android/app/src/main/java/com/garpr/android/views/TimberEntryItemView.java
+++ smash-ranks-android/app/src/main/java/com/garpr/android/views/TimberEntryItemView.java
@@ -3,13 +3,26 @@ package com.garpr.android.views;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.garpr.android.R;
 import com.garpr.android.adapters.BaseAdapterView;
 import com.garpr.android.misc.Timber;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class TimberEntryItemView extends LinearLayout implements BaseAdapterView<Timber.Entry> {
+
+    @BindView(R.id.tvStackTrace)
+    TextView mStackTrace;
+
+    @BindView(R.id.tvTagAndMessage)
+    TextView mTagAndMessage;
+
 
     public TimberEntryItemView(final Context context, final AttributeSet attrs) {
         super(context, attrs);
@@ -27,8 +40,22 @@ public class TimberEntryItemView extends LinearLayout implements BaseAdapterView
     }
 
     @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        ButterKnife.bind(this);
+    }
+
+    @Override
     public void setContent(final Timber.Entry content) {
-        // TODO
+        mTagAndMessage.setText(content.mMsg);
+
+        if (TextUtils.isEmpty(content.mStackTrace)) {
+            mStackTrace.setText("");
+            mStackTrace.setVisibility(GONE);
+        } else {
+            mStackTrace.setText(content.mStackTrace);
+            mStackTrace.setVisibility(VISIBLE);
+        }
     }
 
 }
