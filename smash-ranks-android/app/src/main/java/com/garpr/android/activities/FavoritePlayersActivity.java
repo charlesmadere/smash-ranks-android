f2c4f2954075e65d5aa29352ed8a7f9b7f265b4b
Charles Madere <charlesrmadere@gmail.com>
2017-03-04 22:53:10 -0800

    created new FavoritePlayersActivity

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/activities/FavoritePlayersActivity.java
@@ -0,0 +1,32 @@
+package com.garpr.android.activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class FavoritePlayersActivity extends BaseActivity {
+
+    private static final String TAG = "FavoritePlayersActivity";
+
+
+    public static Intent getLaunchIntent(final Context context) {
+        return new Intent(context, FavoritePlayersActivity.class);
+    }
+
+    @Override
+    protected String getActivityName() {
+        return TAG;
+    }
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected boolean showUpNavigation() {
+        return true;
+    }
+
+}
