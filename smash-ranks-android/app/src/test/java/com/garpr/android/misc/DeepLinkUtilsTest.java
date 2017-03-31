7e80c9a6608409da9bcdc58d106a44a37501095e
Charles Madere <charlesrmadere@gmail.com>
2017-03-30 18:49:14 -0700

    all DeepLinkUtils tests are now fixed

--- smash-ranks-android/app/src/test/java/com/garpr/android/misc/DeepLinkUtilsTest.java
+++ smash-ranks-android/app/src/test/java/com/garpr/android/misc/DeepLinkUtilsTest.java
@@ -1,11 +1,14 @@
 package com.garpr.android.misc;
 
 import android.app.Application;
+import android.content.Intent;
 import android.net.Uri;
 
 import com.garpr.android.BaseTest;
 import com.garpr.android.BuildConfig;
 import com.garpr.android.models.Endpoint;
+import com.garpr.android.models.Region;
+import com.google.gson.Gson;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +20,7 @@ import javax.inject.Inject;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
@@ -41,11 +45,26 @@ public class DeepLinkUtilsTest extends BaseTest {
     private static final String TOURNAMENTS_NORCAL = "https://www.garpr.com/#/norcal/tournaments";
     private static final String TOURNAMENTS_NYC = "https://www.notgarpr.com/#/nyc/tournaments";
 
+    private static final String JSON_REGION_CHICAGO = "{\"ranking_num_tourneys_attended\":2,\"ranking_activity_day_limit\":60,\"display_name\":\"Chicago\",\"id\":\"chicago\",\"tournament_qualified_day_limit\":999}";
+    private static final String JSON_REGION_GEORGIA = "{\"ranking_num_tourneys_attended\":2,\"ranking_activity_day_limit\":75,\"display_name\":\"Georgia\",\"id\":\"georgia\",\"tournament_qualified_day_limit\":180}";
+    private static final String JSON_REGION_GOOGLE_MTV = "{\"ranking_num_tourneys_attended\":1,\"ranking_activity_day_limit\":60,\"display_name\":\"Google MTV\",\"id\":\"googlemtv\",\"tournament_qualified_day_limit\":999}";
+    private static final String JSON_REGION_NORCAL = "{\"ranking_num_tourneys_attended\":2,\"ranking_activity_day_limit\":30,\"display_name\":\"Norcal\",\"id\":\"norcal\",\"tournament_qualified_day_limit\":1000}";
+    private static final String JSON_REGION_NYC = "{\"ranking_num_tourneys_attended\":6,\"ranking_activity_day_limit\":90,\"display_name\":\"NYC Metro Area\",\"id\":\"nyc\",\"tournament_qualified_day_limit\":999}";
+
+    private Region mChicago;
+    private Region mGeorgia;
+    private Region mGoogleMtv;
+    private Region mNorcal;
+    private Region mNyc;
+
     @Inject
     Application mApplication;
 
     @Inject
     DeepLinkUtils mDeepLinkUtils;
+
+    @Inject
+    Gson mGson;
 
 
     @Before
@@ -53,13 +72,17 @@ public class DeepLinkUtilsTest extends BaseTest {
     public void setUp() throws Exception {
         super.setUp();
         getTestAppComponent().inject(this);
+
+        mChicago = mGson.fromJson(JSON_REGION_CHICAGO, Region.class);
+        mGeorgia = mGson.fromJson(JSON_REGION_GEORGIA, Region.class);
+        mGoogleMtv = mGson.fromJson(JSON_REGION_GOOGLE_MTV, Region.class);
+        mNorcal = mGson.fromJson(JSON_REGION_NORCAL, Region.class);
+        mNyc = mGson.fromJson(JSON_REGION_NYC, Region.class);
     }
 
     @Test
     public void testEmptyStringBuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, "");
-//        assertNull(intentStack);
+        assertNull(mDeepLinkUtils.buildIntentStack(mApplication, "", mGoogleMtv));
     }
 
     @Test
@@ -69,163 +92,230 @@ public class DeepLinkUtilsTest extends BaseTest {
 
     @Test
     public void testNullIntentBuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, (Intent) null);
-//        assertNull(intentStack);
+        assertNull(mDeepLinkUtils.buildIntentStack(mApplication, (Intent) null, mGeorgia));
+    }
+
+    @Test
+    public void testNullIntentGetEndpoint() throws Exception {
+        assertNull(mDeepLinkUtils.getEndpoint((Intent) null));
+    }
+
+    @Test
+    public void testNullIntentIsValidUri() throws Exception {
+        assertFalse(mDeepLinkUtils.isValidUri((Intent) null));
     }
 
     @Test
     public void testNullStringBuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, (String) null);
-//        assertNull(intentStack);
+        assertNull(mDeepLinkUtils.buildIntentStack(mApplication, (String) null, mNorcal));
     }
 
     @Test
     public void testNullStringGetEndpoint() throws Exception {
-        assertNull(mDeepLinkUtils.getEndpoint(mApplication, (String) null));
+        assertNull(mDeepLinkUtils.getEndpoint((String) null));
     }
 
     @Test
     public void testNullStringIsValidUri() throws Exception {
-        assertFalse(mDeepLinkUtils.isValidUri(null));
-    }
-
-    @Test
-    public void testNullUriGetEndpoint() throws Exception {
-        assertNull(mDeepLinkUtils.getEndpoint(mApplication, (Uri) null));
+        assertFalse(mDeepLinkUtils.isValidUri((String) null));
     }
 
     @Test
     public void testNullUriBuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, (Uri) null);
-//        assertNull(intentStack);
+        assertNull(mDeepLinkUtils.buildIntentStack(mApplication, (Uri) null, mNyc));
+    }
+
+    @Test
+    public void testNullUriGetEndpoint() throws Exception {
+        assertNull(mDeepLinkUtils.getEndpoint((Uri) null));
+    }
+
+    @Test
+    public void testNullUriIsValidUri() throws Exception {
+        assertFalse(mDeepLinkUtils.isValidUri((Uri) null));
+    }
+
+    @Test
+    public void testPlayerGingerBuildIntentStack() throws Exception {
+        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, PLAYER_GINGER,
+                mChicago);
+        assertNotNull(intentStack);
+        assertEquals(intentStack.length, 3);
+    }
+
+    @Test
+    public void testPlayerGingerGetEndpoint() throws Exception {
+        assertEquals(Endpoint.NOT_GAR_PR, mDeepLinkUtils.getEndpoint(PLAYER_GINGER));
+        assertEquals(Endpoint.NOT_GAR_PR, mDeepLinkUtils.getEndpoint(Uri.parse(PLAYER_GINGER)));
     }
 
     @Test
     public void testPlayerGingerIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(PLAYER_GINGER));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(PLAYER_GINGER)));
     }
 
     @Test
     public void testPlayerSfatBuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, PLAYER_SFAT);
-//        assertNotNull(intentStack);
-//        assertEquals(intentStack.length, 3);
+        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, PLAYER_SFAT,
+                mNorcal);
+        assertNotNull(intentStack);
+        assertEquals(intentStack.length, 3);
     }
 
     @Test
     public void testPlayerSfatIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(PLAYER_SFAT));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(PLAYER_SFAT)));
+    }
+
+    @Test
+    public void testPlayerSwedishDelightBuildIntentStack() throws Exception {
+        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication,
+                PLAYER_SWEDISH_DELIGHT, mNyc);
+        assertNotNull(intentStack);
+        assertEquals(intentStack.length, 3);
     }
 
     @Test
     public void testPlayerSwedishDelightIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(PLAYER_SWEDISH_DELIGHT));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(PLAYER_SWEDISH_DELIGHT)));
     }
 
     @Test
     public void testPlayersGeorgiaIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(PLAYERS_GEORGIA));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(PLAYERS_GEORGIA)));
     }
 
     @Test
     public void testPlayersNorcalBuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, NORCAL_PLAYERS);
-//        assertNotNull(intentStack);
-//        assertEquals(intentStack.length, 2);
+        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, PLAYERS_NORCAL,
+                mNorcal);
+        assertNotNull(intentStack);
+        assertEquals(intentStack.length, 2);
     }
 
     @Test
     public void testPlayersNorcalGetEndpoint() throws Exception {
-        assertEquals(Endpoint.GAR_PR, mDeepLinkUtils.getEndpoint(mApplication, PLAYERS_NORCAL));
+        assertEquals(Endpoint.GAR_PR, mDeepLinkUtils.getEndpoint(PLAYERS_NORCAL));
+        assertEquals(Endpoint.GAR_PR, mDeepLinkUtils.getEndpoint(Uri.parse(PLAYERS_NORCAL)));
     }
 
     @Test
     public void testPlayersNorcalIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(PLAYERS_NORCAL));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(PLAYERS_NORCAL)));
     }
 
     @Test
     public void testRankingsGoogleMtvBuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, GOOGLEMTV_RANKINGS);
-//        assertNotNull(intentStack);
-//        assertEquals(intentStack.length, 2);
+        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication,
+                RANKINGS_GOOGLEMTV, mGoogleMtv);
+        assertNotNull(intentStack);
+        assertEquals(intentStack.length, 2);
     }
 
     @Test
     public void testRankingsGoogleMtvIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(RANKINGS_GOOGLEMTV));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(RANKINGS_GOOGLEMTV)));
     }
 
     @Test
     public void testRankingsLongIslandIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(RANKINGS_LONG_ISLAND));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(RANKINGS_LONG_ISLAND)));
     }
 
     @Test
     public void testRankingsNorcalBuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, NORCAL_RANKINGS);
-//        assertNotNull(intentStack);
-//        assertEquals(intentStack.length, 1);
+        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, RANKINGS_NORCAL,
+                mNorcal);
+        assertNotNull(intentStack);
+        assertEquals(intentStack.length, 1);
+    }
+
+    @Test
+    public void testRankingsNorcalGetEndpoint() throws Exception {
+        assertEquals(Endpoint.GAR_PR, mDeepLinkUtils.getEndpoint(RANKINGS_NORCAL));
+        assertEquals(Endpoint.GAR_PR, mDeepLinkUtils.getEndpoint(Uri.parse(RANKINGS_NORCAL)));
     }
 
     @Test
     public void testRankingsNorcalIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(RANKINGS_NORCAL));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(RANKINGS_NORCAL)));
+    }
+
+    @Test
+    public void testTournamentApolloIiiBuildIntentStack() throws Exception {
+        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication,
+                TOURNAMENT_APOLLO_III, mNyc);
+        assertNotNull(intentStack);
+        assertEquals(intentStack.length, 3);
     }
 
     @Test
     public void testTournamentApolloIiiIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(TOURNAMENT_APOLLO_III));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(TOURNAMENT_APOLLO_III)));
     }
 
     @Test
     public void testTournamentNorcalValidated2IsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(TOURNAMENT_NORCAL_VALIDATED_2));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(TOURNAMENT_NORCAL_VALIDATED_2)));
     }
 
     @Test
     public void testTournamentNorcalValidated2BuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication,
-//                TOURNAMENT_NORCAL_VALIDATED_2);
-//
-//        assertNotNull(intentStack);
-//        assertEquals(intentStack.length, 2);
+        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication,
+                TOURNAMENT_NORCAL_VALIDATED_2, mNorcal);
+        assertNotNull(intentStack);
+        assertEquals(intentStack.length, 2);
     }
 
     @Test
     public void testTournamentsNorcalIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(TOURNAMENTS_NORCAL));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(TOURNAMENTS_NORCAL)));
     }
 
     @Test
     public void testTournamentsNycBuildIntentStack() throws Exception {
-        // TODO
+        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, TOURNAMENTS_NYC,
+                mNyc);
+        assertNotNull(intentStack);
+        assertEquals(intentStack.length, 2);
     }
 
     @Test
     public void testTournamentsNycGetEndpoint() throws Exception {
-        assertEquals(Endpoint.NOT_GAR_PR, mDeepLinkUtils.getEndpoint(mApplication,
-                TOURNAMENTS_NYC));
+        assertEquals(Endpoint.NOT_GAR_PR, mDeepLinkUtils.getEndpoint(TOURNAMENTS_NYC));
+        assertEquals(Endpoint.NOT_GAR_PR, mDeepLinkUtils.getEndpoint(Uri.parse(TOURNAMENTS_NYC)));
     }
 
     @Test
     public void testTournamentsNycIsValidUri() throws Exception {
         assertTrue(mDeepLinkUtils.isValidUri(TOURNAMENTS_NYC));
+        assertTrue(mDeepLinkUtils.isValidUri(Uri.parse(TOURNAMENTS_NYC)));
     }
 
     @Test
     public void testWhitespaceStringBuildIntentStack() throws Exception {
-        // TODO
-//        final Intent[] intentStack = mDeepLinkUtils.buildIntentStack(mApplication, " ");
-//        assertNull(intentStack);
+        assertNull(mDeepLinkUtils.buildIntentStack(mApplication, " ", mGeorgia));
+    }
+
+    @Test
+    public void testWhitespaceStringGetEndpoint() throws Exception {
+        assertNull(mDeepLinkUtils.getEndpoint(" "));
+    }
+
+    @Test
+    public void testWhitespaceStringIsValidUri() throws Exception {
+        assertFalse(mDeepLinkUtils.isValidUri(" "));
     }
 
 }
