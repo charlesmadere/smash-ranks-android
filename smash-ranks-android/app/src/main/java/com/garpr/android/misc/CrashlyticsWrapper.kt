a831ac6e9af5c7e824c086834e8c0dc39d2179af
Charles Madere <charlesrmadere@gmail.com>
2017-06-27 14:55:38 -0700

    converted some classes to Kotlin

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/misc/CrashlyticsWrapper.kt
@@ -0,0 +1,17 @@
+package com.garpr.android.misc
+
+interface CrashlyticsWrapper {
+
+    fun initialize(disabled: Boolean)
+
+    fun log(priority: Int, tag: String, msg: String)
+
+    fun logException(tr: Throwable)
+
+    fun setBool(key: String, value: Boolean)
+
+    fun setInt(key: String, value: Int)
+
+    fun setString(key: String, value: String)
+
+}
