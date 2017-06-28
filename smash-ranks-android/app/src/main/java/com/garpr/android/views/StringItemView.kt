71db459038d3b6e797cbcfa74d253ce282caa4b5
Charles Madere <charlesrmadere@gmail.com>
2017-06-28 16:42:13 -0700

    converted StringItemView into Kotlin

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/views/StringItemView.kt
@@ -0,0 +1,21 @@
+package com.garpr.android.views
+
+import android.content.Context
+import android.support.annotation.AttrRes
+import android.support.v7.widget.AppCompatTextView
+import android.util.AttributeSet
+
+import com.garpr.android.adapters.BaseAdapterView
+
+class StringItemView : AppCompatTextView, BaseAdapterView<String> {
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, @AttrRes defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr)
+
+    override fun setContent(content: String) {
+        text = content
+    }
+
+}
