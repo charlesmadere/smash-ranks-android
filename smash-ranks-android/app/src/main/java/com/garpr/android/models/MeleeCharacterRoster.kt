28f0e4d7f952e30ad07eb7764196eb4e9bd0a492
Charles Madere <charlesrmadere@gmail.com>
2017-09-26 22:45:58 -0700

    MeleeCharacterRoster class now includes the roster

--- smash-ranks-android/app/src/main/java/com/garpr/android/models/MeleeCharacterRoster.kt
+++ smash-ranks-android/app/src/main/java/com/garpr/android/models/MeleeCharacterRoster.kt
@@ -3,20 +3,25 @@ package com.garpr.android.models
 import android.os.Parcel
 import android.os.Parcelable
 import com.garpr.android.extensions.createParcel
+import com.garpr.android.extensions.readStringMap
+import com.garpr.android.extensions.writeStringMap
 
 data class MeleeCharacterRoster(
+        val players: Map<String, String>?,
         val regionId: String,
         val version: Int
 ) : Parcelable {
 
     companion object {
         @JvmField
-        val CREATOR = createParcel { MeleeCharacterRoster(it.readString(), it.readInt()) }
+        val CREATOR = createParcel { MeleeCharacterRoster(it.readStringMap(), it.readString(),
+                it.readInt()) }
     }
 
     override fun describeContents() = 0
 
     override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeStringMap(players)
         dest.writeString(regionId)
         dest.writeInt(version)
     }
