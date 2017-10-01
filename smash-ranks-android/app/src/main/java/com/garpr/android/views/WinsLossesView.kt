923c49ceb6109198eae5b31df2948261efa77676
Charles Madere <charlesrmadere@gmail.com>
2017-09-30 17:55:02 -0700

    set up the paint variables

--- smash-ranks-android/app/src/main/java/com/garpr/android/views/WinsLossesView.kt
+++ smash-ranks-android/app/src/main/java/com/garpr/android/views/WinsLossesView.kt
@@ -3,10 +3,12 @@ package com.garpr.android.views
 import android.annotation.TargetApi
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.graphics.RectF
 import android.os.Build
 import android.support.annotation.AttrRes
 import android.support.annotation.StyleableRes
+import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import com.garpr.android.R
@@ -18,8 +20,10 @@ import kotterknife.bindView
 class WinsLossesView : LinearLayout, BaseAdapterView<WinsLosses> {
 
     private var mContent: WinsLosses? = null
-    private val mPlayerWinsRect = RectF()
-    private val mOpponentWinsRect = RectF()
+    private val mPlayerPaint = Paint()
+    private var mOpponentArcEnd: Float = 0f
+    private val mOpponentPaint = Paint()
+    private val mRect = RectF()
 
     private val mPlayerColumnView: WinsLossesColumnView by bindView(R.id.playerColumnView)
     private val mOpponentColumnView: WinsLossesColumnView by bindView(R.id.opponentColumnView)
@@ -35,14 +39,32 @@ class WinsLossesView : LinearLayout, BaseAdapterView<WinsLosses> {
             @StyleableRes defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
     private fun calculateRects() {
-        val h = height
-        val w = width
         val c = mContent
 
-        if (h == 0 || w == 0 || c == null) {
-            mPlayerWinsRect.setEmpty()
-            mOpponentWinsRect.setEmpty()
+        if (height == 0 || width == 0 || c == null) {
+            mRect.setEmpty()
             return
+        }
+
+        val h: Float = height.toFloat() / 4f
+        val w: Float = width.toFloat() / 4f
+
+        mRect.set(w, h, w * 3f, h * 3f)
+
+        val percentPlayerWins: Float
+        val percentOpponentWins: Float
+
+        if (c.playerWins == 0) {
+            percentPlayerWins = 0f
+            percentOpponentWins = 1f
+            mOpponentArcEnd = 360f
+        } else if (c.opponentWins == 0) {
+            percentPlayerWins = 1f
+            percentOpponentWins = 0f
+            mOpponentArcEnd = 0f
+        } else {
+            percentPlayerWins = c.playerWins.toFloat() / (c.playerWins + c.opponentWins).toFloat()
+            percentOpponentWins = 1f - percentPlayerWins
         }
 
         // TODO
@@ -56,7 +78,7 @@ class WinsLossesView : LinearLayout, BaseAdapterView<WinsLosses> {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        if (mPlayerWinsRect.isEmpty && mOpponentWinsRect.isEmpty) {
+        if (mRect.isEmpty) {
             return
         }
 
@@ -70,6 +92,16 @@ class WinsLossesView : LinearLayout, BaseAdapterView<WinsLosses> {
             setContent(WinsLosses(LitePlayer("0", "PewPewU"), 8,
                     LitePlayer("1", "Shroomed"), 5))
         }
+
+        mPlayerPaint.color = ContextCompat.getColor(context, R.color.win_background)
+        mPlayerPaint.isAntiAlias = true
+        mPlayerPaint.isDither = true
+        mPlayerPaint.style = Paint.Style.FILL
+
+        mOpponentPaint.color = ContextCompat.getColor(context, R.color.lose_background)
+        mOpponentPaint.isAntiAlias = true
+        mOpponentPaint.isDither = true
+        mOpponentPaint.style = Paint.Style.FILL
     }
 
     override fun setContent(content: WinsLosses) {
