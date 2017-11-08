ae0f331e3af0e31755bca68f271b86fcd63d4531
Charles Madere <charlesrmadere@gmail.com>
2017-11-07 19:16:43 -0800

    added some debug stuff to NotificationsManagerImpl

--- smash-ranks-android/app/src/main/java/com/garpr/android/misc/NotificationsManagerImpl.kt
+++ smash-ranks-android/app/src/main/java/com/garpr/android/misc/NotificationsManagerImpl.kt
@@ -1,7 +1,6 @@
 package com.garpr.android.misc
 
 import android.app.*
-import android.content.Context
 import android.os.Build
 import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
@@ -9,6 +8,7 @@ import com.garpr.android.R
 import com.garpr.android.activities.HomeActivity
 import com.garpr.android.extensions.notificationManager
 import com.garpr.android.extensions.notificationManagerCompat
+import com.garpr.android.models.SimpleDate
 import com.garpr.android.preferences.RankingsPollingPreferenceStore
 
 class NotificationsManagerImpl(
@@ -21,12 +21,12 @@ class NotificationsManagerImpl(
     private val mImpl = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) Api26Impl() else BaseImpl()
 
 
-    private open class BaseImpl {
-        open fun createBuilder(context: Context): NotificationCompat.Builder {
-            return NotificationCompat.Builder(context, RANKINGS_CHANNEL)
+    private inner open class BaseImpl {
+        open fun createBuilder(): NotificationCompat.Builder {
+            return NotificationCompat.Builder(mApplication, RANKINGS_CHANNEL)
                     .setAutoCancel(true)
                     .setCategory(NotificationCompat.CATEGORY_SOCIAL)
-                    .setContentTitle(context.getString(R.string.gar_pr))
+                    .setContentTitle(mApplication.getString(R.string.gar_pr))
                     .setPriority(NotificationCompat.PRIORITY_LOW)
                     .setSmallIcon(R.drawable.notification)
                     .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
@@ -34,18 +34,18 @@ class NotificationsManagerImpl(
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private class Api26Impl : BaseImpl() {
-        override fun createBuilder(context: Context): NotificationCompat.Builder {
+    private inner class Api26Impl : BaseImpl() {
+        override fun createBuilder(): NotificationCompat.Builder {
             val notificationChannel = NotificationChannel(RANKINGS_CHANNEL,
-                    context.getString(R.string.rankings_update_notifications),
+                    mApplication.getString(R.string.rankings_update_notifications),
                     NotificationManager.IMPORTANCE_LOW)
-            notificationChannel.description = context.getString(R.string.rankings_update_description)
+            notificationChannel.description = mApplication.getString(R.string.rankings_update_description)
             notificationChannel.enableLights(true)
             notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
             notificationChannel.setShowBadge(true)
-            context.notificationManager.createNotificationChannel(notificationChannel)
+            mApplication.notificationManager.createNotificationChannel(notificationChannel)
 
-            return super.createBuilder(context)
+            return super.createBuilder()
         }
     }
 
@@ -61,13 +61,14 @@ class NotificationsManagerImpl(
     }
 
     override fun rankingsUpdated() {
-        val builder = mImpl.createBuilder(mApplication)
+        val builder = mImpl.createBuilder()
 
         builder.setContentIntent(PendingIntent.getActivity(mApplication, 0,
                 HomeActivity.getLaunchIntent(mApplication), PendingIntent.FLAG_UPDATE_CURRENT))
 
+        val regionDisplayName = mRegionManager.getRegion().displayName
         builder.setContentText(mApplication.getString(R.string.x_rankings_have_been_updated,
-                mRegionManager.getRegion().displayName))
+                regionDisplayName))
 
         if (mRankingsPollingPreferenceStore.vibrationEnabled.get() == true) {
             builder.setDefaults(NotificationCompat.DEFAULT_LIGHTS or NotificationCompat.DEFAULT_VIBRATE)
@@ -79,12 +80,19 @@ class NotificationsManagerImpl(
             builder.setSound(it)
         }
 
-        mRankingsPollingPreferenceStore.rankingsDate.get()?.let {
+        val rankingsDate = mRankingsPollingPreferenceStore.rankingsDate.get()?.let {
             builder.setShowWhen(true)
                     .setWhen(it.date.time)
-        }
+            it
+        } ?: "null"
 
-        mTimber.d(TAG, "showing rankings updated notification")
+        val time = SimpleDate()
+
+        mTimber.d(TAG, "Showing rankings updated notification! Debug info: " +
+                "regionDisplayName = $regionDisplayName, " +
+                "rankingsDate = $rankingsDate, " +
+                "time = $time")
+
         show(RANKINGS_ID, builder)
     }
 
