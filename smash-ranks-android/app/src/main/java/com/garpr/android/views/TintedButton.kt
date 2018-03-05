0fd8f58b78102af6964ed9f18cd9935137bbe4d4
Charles Madere <charlesrmadere@gmail.com>
2018-03-04 23:49:13 -0800

    new LifecycleButton and TintedButton

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/views/TintedButton.kt
@@ -0,0 +1,47 @@
+package com.garpr.android.views
+
+import android.content.Context
+import android.support.annotation.AttrRes
+import android.support.annotation.ColorInt
+import android.util.AttributeSet
+import com.garpr.android.R
+import com.garpr.android.extensions.getAttrColor
+import com.garpr.android.extensions.setTintedImageColor
+import com.garpr.android.misc.Refreshable
+
+class TintedButton @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        @AttrRes defStyleAttr: Int = 0
+) : LifecycleButton(context, attrs, defStyleAttr), Refreshable {
+
+    @ColorInt
+    private var drawableTintColor: Int = 0
+
+
+    init {
+        parseAttributes(attrs)
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        refresh()
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        refresh()
+    }
+
+    private fun parseAttributes(attrs: AttributeSet?) {
+        val ta = context.obtainStyledAttributes(attrs, R.styleable.View)
+        drawableTintColor = ta.getColor(R.styleable.View_drawableTintColor,
+                context.getAttrColor(android.R.attr.textColorSecondary))
+        ta.recycle()
+    }
+
+    override fun refresh() {
+        setTintedImageColor(drawableTintColor)
+    }
+
+}
