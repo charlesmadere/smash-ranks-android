e6a5c6c185ae6583cfa85e9cbf4fa825c2d7dae0
Charles Madere <charlesrmadere@gmail.com>
2018-03-04 19:43:11 -0800

    progress on PlayerProfileManagerTest

--- smash-ranks-android/app/src/test/java/com/garpr/android/misc/PlayerProfileManagerTest.kt
+++ smash-ranks-android/app/src/test/java/com/garpr/android/misc/PlayerProfileManagerTest.kt
@@ -4,6 +4,7 @@ import com.garpr.android.BaseTest
 import com.garpr.android.models.AbsRegion
 import com.garpr.android.models.FullPlayer
 import com.garpr.android.models.LiteRegion
+import com.garpr.android.models.Rating
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -26,10 +27,25 @@ class PlayerProfileManagerTest : BaseTest() {
 
 
     companion object {
-        private val FULL_PLAYER_1 = FullPlayer("1", "Charlezard", listOf("charles"),
+        private val FULL_PLAYER_1 = FullPlayer("1", "Imyt", null,
                 null, null)
-        private val FULL_PLAYER_2 = FullPlayer("2", "Imyt", null,
-                null, null)
+
+        private val FULL_PLAYER_2 = FullPlayer("2", "Charlezard", listOf("charles"),
+                null, mapOf(
+                        "norcal" to Rating(10f, 2f)
+        ))
+
+        private val FULL_PLAYER_3 = FullPlayer("3", "Hax", listOf("hax$"),
+                null, mapOf(
+                "norcal" to Rating(20f, 1.5f),
+                "nyc" to Rating(25f, 1f)
+        ))
+
+        private val FULL_PLAYER_4 = FullPlayer("4", "gaR", listOf(),
+                null, mapOf(
+                "norcal" to Rating(20f, 1.5f),
+                "googlemtv" to Rating(25f, 1f)
+        ))
 
         private val REGION_1: AbsRegion = LiteRegion(null, null,
                 null, null, "Norcal",
@@ -49,6 +65,36 @@ class PlayerProfileManagerTest : BaseTest() {
     @Test
     fun testFullPlayer1() {
         val presentation = playerProfileManager.getPresentation(FULL_PLAYER_1, REGION_1)
+        assertTrue(presentation.isAddToFavoritesVisible)
+        assertFalse(presentation.isViewYourselfVsThisOpponentVisible)
+        assertTrue(presentation.aliases.isNullOrBlank())
+        assertTrue(presentation.rating.isNullOrBlank())
+        assertTrue(presentation.unadjustedRating.isNullOrBlank())
+    }
+
+    @Test
+    fun testFullPlayer2() {
+        val presentation = playerProfileManager.getPresentation(FULL_PLAYER_2, REGION_1)
+        assertTrue(presentation.isAddToFavoritesVisible)
+        assertFalse(presentation.isViewYourselfVsThisOpponentVisible)
+        assertTrue(presentation.aliases.isNullOrBlank())
+        assertTrue(presentation.rating.isNullOrBlank())
+        assertTrue(presentation.unadjustedRating.isNullOrBlank())
+    }
+
+    @Test
+    fun testFullPlayer3() {
+        val presentation = playerProfileManager.getPresentation(FULL_PLAYER_3, REGION_2)
+        assertTrue(presentation.isAddToFavoritesVisible)
+        assertFalse(presentation.isViewYourselfVsThisOpponentVisible)
+        assertTrue(presentation.aliases.isNullOrBlank())
+        assertTrue(presentation.rating.isNullOrBlank())
+        assertTrue(presentation.unadjustedRating.isNullOrBlank())
+    }
+
+    @Test
+    fun testFullPlayer4() {
+        val presentation = playerProfileManager.getPresentation(FULL_PLAYER_4, REGION_1)
         assertTrue(presentation.isAddToFavoritesVisible)
         assertFalse(presentation.isViewYourselfVsThisOpponentVisible)
         assertTrue(presentation.aliases.isNullOrBlank())
