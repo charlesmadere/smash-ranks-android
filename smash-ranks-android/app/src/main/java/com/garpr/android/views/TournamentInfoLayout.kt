9e89a6b406c8f6e657c7713aff96d4583007c6c2
Charles Madere <charlesrmadere@gmail.com>
2018-03-04 23:35:12 -0800

    filling in layout

--- smash-ranks-android/app/src/main/java/com/garpr/android/views/TournamentInfoLayout.kt
+++ smash-ranks-android/app/src/main/java/com/garpr/android/views/TournamentInfoLayout.kt
@@ -1,21 +1,31 @@
 package com.garpr.android.views
 
+import android.annotation.TargetApi
 import android.content.Context
+import android.os.Build
 import android.support.annotation.AttrRes
+import android.support.annotation.StyleRes
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import com.garpr.android.R
 import com.garpr.android.models.FullTournament
+import kotterknife.bindView
 
-class TournamentInfoLayout @JvmOverloads constructor(
-        context: Context,
-        attrs: AttributeSet? = null,
-        @AttrRes defStyleAttr: Int = 0
-) : LifecycleConstraintLayout(context, attrs, defStyleAttr), TournamentPagerAdapterView {
+class TournamentInfoLayout : LifecycleScrollView, TournamentPagerAdapterView {
 
+    private val name: TextView by bindView(R.id.tvName)
 
 
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet?, @AttrRes defStyleAttr: Int) :
+            super(context, attrs, defStyleAttr)
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(context: Context, attrs: AttributeSet?, @AttrRes defStyleAttr: Int,
+                @StyleRes defStyleRes: Int) : super(context, attrs, defStyleAttr, defStyleRes)
 
     companion object {
         fun inflate(parent: ViewGroup): TournamentInfoLayout = LayoutInflater.from(
