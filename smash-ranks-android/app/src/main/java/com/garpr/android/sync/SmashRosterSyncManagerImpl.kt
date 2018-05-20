58f90ec094bc13c06114c8ba09540f5436354030
Charles Madere <charlesrmadere@gmail.com>
2018-05-19 19:07:57 -0700

    sync frequency changed to weekly

--- smash-ranks-android/app/src/main/java/com/garpr/android/sync/SmashRosterSyncManagerImpl.kt
+++ smash-ranks-android/app/src/main/java/com/garpr/android/sync/SmashRosterSyncManagerImpl.kt
@@ -83,7 +83,7 @@ class SmashRosterSyncManagerImpl(
                 .setService(SmashRosterSyncJobService::class.java)
                 .setTag(TAG)
                 .setTrigger(Trigger.executionWindow(TimeUnit.MINUTES.toSeconds(5).toInt(),
-                        PollFrequency.EVERY_3_DAYS.timeInSeconds.toInt()))
+                        PollFrequency.WEEKLY.timeInSeconds.toInt()))
 
         jobDispatcher.mustSchedule(jobBuilder.build())
         timber.d(TAG, "sync has been enabled")
