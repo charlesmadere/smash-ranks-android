2e1e4f71b319335b9f86da289971e514ed038596
Charles Madere <charlesrmadere@gmail.com>
2018-05-19 20:08:46 -0700

    SmashRosterSyncJobService now implemented

--- smash-ranks-android/app/src/main/java/com/garpr/android/sync/SmashRosterSyncJobService.kt
+++ smash-ranks-android/app/src/main/java/com/garpr/android/sync/SmashRosterSyncJobService.kt
@@ -4,13 +4,14 @@ import com.firebase.jobdispatcher.JobParameters
 import com.firebase.jobdispatcher.JobService
 import com.garpr.android.extensions.appComponent
 import com.garpr.android.misc.Timber
-import com.garpr.android.networking.ApiListener
 import javax.inject.Inject
 
-class SmashRosterSyncJobService : JobService(), ApiListener<Boolean> {
+class SmashRosterSyncJobService : JobService(), SmashRosterSyncManager.OnSyncListeners {
 
-    private var _isAlive: Boolean = true
     private var jobParameters: JobParameters? = null
+
+    @Inject
+    protected lateinit var smashRosterSyncManager: SmashRosterSyncManager
 
     @Inject
     protected lateinit var timber: Timber
@@ -20,42 +21,36 @@ class SmashRosterSyncJobService : JobService(), ApiListener<Boolean> {
         private const val TAG = "SmashRosterSyncJobService"
     }
 
-    override fun failure(errorCode: Int) {
-        // TODO
-    }
-
-    override val isAlive: Boolean
-        get() = _isAlive
-
-    private fun jobFinished(needsReschedule: Boolean) {
-        timber.d(TAG, "job finished... needs reschedule: $needsReschedule")
-        jobParameters?.let { jobFinished(it, needsReschedule) }
-    }
-
     override fun onCreate() {
         super.onCreate()
         appComponent.inject(this)
     }
 
+    override fun onSmashRosterSyncBegin(smashRosterSyncManager: SmashRosterSyncManager) {
+        timber.d(TAG, "sync beginning")
+    }
+
+    override fun onSmashRosterSyncComplete(smashRosterSyncManager: SmashRosterSyncManager) {
+        val success: Boolean = smashRosterSyncManager.syncResult?.success == true
+        timber.d(TAG, "sync complete... was successful: $success")
+        jobParameters?.let { jobFinished(it, !success) }
+    }
+
     override fun onStartJob(job: JobParameters): Boolean {
         timber.d(TAG, "starting job...")
-
         jobParameters = job
 
-        // TODO
-        return false
+        smashRosterSyncManager.addListener(this)
+        smashRosterSyncManager.sync()
+
+        return true
     }
 
     override fun onStopJob(job: JobParameters): Boolean {
-        _isAlive = false
-        timber.d(TAG, "stopping job... retry: ")
-
-        // TODO
-        return false
-    }
-
-    override fun success(`object`: Boolean?) {
-        // TODO
+        val success: Boolean = smashRosterSyncManager.syncResult?.success == true
+        timber.d(TAG, "stopping job... was successful: $success")
+        smashRosterSyncManager.removeListener(this)
+        return !success
     }
 
 }
