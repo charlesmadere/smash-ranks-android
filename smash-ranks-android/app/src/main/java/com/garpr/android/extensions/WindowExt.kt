49fdac743d817fddb4ea6f520be5883e6ad723d9
Charles Madere <charlesrmadere@gmail.com>
2018-06-13 20:59:24 -0700

    statusBarColorCompat is now more useful

--- smash-ranks-android/app/src/main/java/com/garpr/android/extensions/WindowExt.kt
+++ smash-ranks-android/app/src/main/java/com/garpr/android/extensions/WindowExt.kt
@@ -1,11 +1,18 @@
 package com.garpr.android.extensions
 
 import android.os.Build
+import android.support.v4.content.ContextCompat
 import android.view.Window
+import com.garpr.android.R
 
-val Window.statusBarColorCompat: Int?
+var Window.statusBarColorCompat: Int?
     get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 statusBarColor
             } else {
                 null
             }
+    set(value) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            statusBarColor = value ?: ContextCompat.getColor(context, R.color.transparent)
+        }
+    }
