7a10a4ad38083ff33e7efa143fb27d022ea0bca2
Charles Madere <charlesrmadere@gmail.com>
2018-07-11 18:58:06 -0700

    better exception messages

--- smash-ranks-android/app/src/main/java/com/garpr/android/extensions/Intent.kt
+++ smash-ranks-android/app/src/main/java/com/garpr/android/extensions/Intent.kt
@@ -2,10 +2,14 @@ package com.garpr.android.extensions
 
 import android.content.Intent
 
-fun Intent.requireStringExtra(name: String): String {
-    if (!hasExtra(name)) {
-        throw RuntimeException("Intent does not have String extra: \"$name\"")
+fun Intent?.requireStringExtra(name: String): String {
+    if (this == null) {
+        throw NullPointerException("Intent is null")
     }
 
-    return getStringExtra(name) ?: throw NullPointerException("Intent String extra (\"$name\") is null")
+    if (!hasExtra(name)) {
+        throw RuntimeException("Intent does not have String extra \"$name\"")
+    }
+
+    return getStringExtra(name) ?: throw NullPointerException("Intent String extra \"$name\" is null")
 }
