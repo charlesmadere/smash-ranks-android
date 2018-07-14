e9a8586167471875d40a02fbedc7a807c8d360dc
Charles Madere <charlesrmadere@gmail.com>
2018-07-14 12:54:09 -0700

    fixed HomeToolbarManager tests

--- smash-ranks-android/app/src/test/java/com/garpr/android/managers/HomeToolbarManagerTest.kt
+++ smash-ranks-android/app/src/test/java/com/garpr/android/managers/HomeToolbarManagerTest.kt
@@ -1,8 +1,9 @@
 package com.garpr.android.managers
 
-import android.app.Application
 import com.garpr.android.BaseTest
 import com.garpr.android.models.AbsPlayer
+import com.garpr.android.models.Endpoint
+import com.garpr.android.models.LiteRegion
 import com.garpr.android.models.Region
 import com.google.gson.Gson
 import org.junit.Assert.assertFalse
@@ -18,85 +19,98 @@ class HomeToolbarManagerTest : BaseTest() {
 
     private lateinit var hax: AbsPlayer
     private lateinit var imyt: AbsPlayer
-    private lateinit var norcal: Region
-    private lateinit var nyc: Region
-
-    @Inject
-    protected lateinit var application: Application
-
-    @Inject
-    protected lateinit var homeToolbarManager: HomeToolbarManager
 
     @Inject
     protected lateinit var gson: Gson
 
     @Inject
-    protected lateinit var identityManager: IdentityManager
+    protected lateinit var homeToolbarManager: HomeToolbarManager
 
     @Inject
-    protected lateinit var regionManager: RegionManager
+    protected lateinit var identityManager: IdentityManager
 
 
     companion object {
         private const val JSON_PLAYER_HAX = "{\"id\":\"53c64dba8ab65f6e6651f7bc\",\"name\":\"Hax\",\"rank\":2,\"rating\":37.975921649503086}"
         private const val JSON_PLAYER_IMYT = "{\"ratings\":{\"norcal\":{\"mu\":37.52665547291405,\"sigma\":0.9061366338189941}},\"name\":\"Imyt\",\"regions\":[\"norcal\"],\"merge_children\":[\"5877eb55d2994e15c7dea98b\"],\"id\":\"5877eb55d2994e15c7dea98b\",\"merged\":false,\"merge_parent\":null}"
-        private const val JSON_REGION_NORCAL = "{\"ranking_num_tourneys_attended\":2,\"ranking_activity_day_limit\":45,\"display_name\":\"Norcal\",\"id\":\"norcal\",\"tournament_qualified_day_limit\":1000,\"endpoint\":\"gar_pr\"}"
-        private const val JSON_REGION_NYC = "{\"display_name\":\"NYC Metro Area\",\"id\":\"nyc\",\"tournament_qualified_day_limit\":90,\"endpoint\":\"not_gar_pr\"}"
+
+        private val GOOGLE_MTV = LiteRegion(
+                activeTf = null,
+                rankingActivityDayLimit = null,
+                rankingNumTourneysAttended = 1,
+                tournamentQualifiedDayLimit = 60,
+                displayName = "Google MTV",
+                id = "googlemtv"
+        )
+
+        private val NORCAL = Region(
+                displayName = "Norcal",
+                id = "norcal",
+                endpoint = Endpoint.GAR_PR
+        )
+
+        private val NYC = Region(
+                activeTf = null,
+                rankingActivityDayLimit = 60,
+                rankingNumTourneysAttended = 2,
+                tournamentQualifiedDayLimit = 90,
+                displayName = "New York City",
+                id = "nyc",
+                endpoint = Endpoint.NOT_GAR_PR
+        )
     }
 
     @Before
-    @Throws(Exception::class)
     override fun setUp() {
         super.setUp()
         testAppComponent.inject(this)
 
         hax = gson.fromJson(JSON_PLAYER_HAX, AbsPlayer::class.java)
         imyt = gson.fromJson(JSON_PLAYER_IMYT, AbsPlayer::class.java)
-        norcal = gson.fromJson(JSON_REGION_NORCAL, Region::class.java)
-        nyc = gson.fromJson(JSON_REGION_NYC, Region::class.java)
     }
 
     @Test
-    @Throws(Exception::class)
-    fun testGetPresentationWithNorcalWithIdentity() {
-        identityManager.setIdentity(imyt, norcal)
-        regionManager.setRegion(norcal)
-
-        val presentation = homeToolbarManager.getPresentation(application)
-        assertTrue(presentation.isActivityRequirementsVisible)
-        assertTrue(presentation.isViewYourselfVisible)
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun testGetPresentationWithNorcalWithoutIdentity() {
+    fun testGetPresentationWithGoogleMtv() {
         identityManager.removeIdentity()
-        regionManager.setRegion(norcal)
 
-        val presentation = homeToolbarManager.getPresentation(application)
-        assertTrue(presentation.isActivityRequirementsVisible)
+        val presentation = homeToolbarManager.getPresentation(GOOGLE_MTV)
+        assertFalse(presentation.isActivityRequirementsVisible)
         assertFalse(presentation.isViewYourselfVisible)
     }
 
     @Test
-    @Throws(Exception::class)
-    fun testGetPresentationWithNycWithHax() {
-        identityManager.setIdentity(hax, nyc)
-        regionManager.setRegion(nyc)
+    fun testGetPresentationWithNorcalWithIdentityImyt() {
+        identityManager.setIdentity(imyt, NORCAL)
 
-        val presentation = homeToolbarManager.getPresentation(application)
+        val presentation = homeToolbarManager.getPresentation(NORCAL)
         assertFalse(presentation.isActivityRequirementsVisible)
         assertTrue(presentation.isViewYourselfVisible)
     }
 
     @Test
-    @Throws(Exception::class)
+    fun testGetPresentationWithNorcalWithoutIdentity() {
+        identityManager.removeIdentity()
+
+        val presentation = homeToolbarManager.getPresentation(NORCAL)
+        assertFalse(presentation.isActivityRequirementsVisible)
+        assertFalse(presentation.isViewYourselfVisible)
+    }
+
+    @Test
+    fun testGetPresentationWithNycWithIdentityHax() {
+        identityManager.setIdentity(hax, NYC)
+
+        val presentation = homeToolbarManager.getPresentation(NYC)
+        assertTrue(presentation.isActivityRequirementsVisible)
+        assertTrue(presentation.isViewYourselfVisible)
+    }
+
+    @Test
     fun testGetPresentationWithNycWithoutIdentity() {
         identityManager.removeIdentity()
-        regionManager.setRegion(nyc)
 
-        val presentation = homeToolbarManager.getPresentation(application)
-        assertFalse(presentation.isActivityRequirementsVisible)
+        val presentation = homeToolbarManager.getPresentation(NYC)
+        assertTrue(presentation.isActivityRequirementsVisible)
         assertFalse(presentation.isViewYourselfVisible)
     }
 
