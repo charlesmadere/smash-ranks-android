64df60235a9539802f5f20cc78a0bc22f0405f5a
Charles Madere <charlesrmadere@gmail.com>
2018-10-14 02:28:07 -0700

    use Kotlin’s NotImplementedError exception

--- smash-ranks-android/app/src/test/java/com/garpr/android/networking/AbsServerApi.kt
+++ smash-ranks-android/app/src/test/java/com/garpr/android/networking/AbsServerApi.kt
@@ -6,46 +6,46 @@ abstract class AbsServerApi : ServerApi {
 
     override fun getHeadToHead(region: Region, playerId: String, opponentId: String,
             listener: ApiListener<HeadToHead>) {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
     override fun getMatches(region: Region, playerId: String,
             listener: ApiListener<MatchesBundle>) {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
     override fun getPlayer(region: Region, playerId: String, listener: ApiListener<FullPlayer>) {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
     override fun getPlayerMatches(region: Region, playerId: String,
             listener: ApiListener<PlayerMatchesBundle>) {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
     override fun getPlayers(region: Region, listener: ApiListener<PlayersBundle>) {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
     override fun getRankings(region: Region, listener: ApiListener<RankingsBundle>) {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
     override fun getRegions(endpoint: Endpoint?, listener: ApiListener<RegionsBundle>) {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
     override fun getSmashRoster(endpoint: Endpoint): ServerResponse<Map<String, SmashCompetitor>> {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
     override fun getTournament(region: Region, tournamentId: String,
             listener: ApiListener<FullTournament>) {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
     override fun getTournaments(region: Region, listener: ApiListener<TournamentsBundle>) {
-        throw RuntimeException()
+        throw NotImplementedError()
     }
 
 }
