95e623181d613be60b9a5cd59ae8114f2c57e389
Charles Madere <charlesmadere@users.noreply.github.com>
2018-12-09 16:31:27 -0800

    tiny test cleanup

--- smash-ranks-android/app/src/test/java/com/garpr/android/models/SmashCompetitorTest.kt
+++ smash-ranks-android/app/src/test/java/com/garpr/android/models/SmashCompetitorTest.kt
@@ -15,25 +15,29 @@ class SmashCompetitorTest : BaseTest() {
         private val SMASH_COMPETITOR_1 = SmashCompetitor(
                 id = "1",
                 name = "Charles",
-                tag = "Charlezard")
+                tag = "Charlezard"
+        )
 
         private val SMASH_COMPETITOR_2 = SmashCompetitor(
                 id = "2",
                 name = "Declan",
                 tag = "Imyt",
-                mains = listOf(SmashCharacter.SHEIK, SmashCharacter.FALCO, SmashCharacter.FOX))
+                mains = listOf(SmashCharacter.SHEIK, SmashCharacter.FALCO, SmashCharacter.FOX)
+        )
 
         private val SMASH_COMPETITOR_3 = SmashCompetitor(
                 id = "3",
                 name = "Ivan",
                 tag = "gaR",
-                mains = listOf())
+                mains = emptyList()
+        )
 
         private val SMASH_COMPETITOR_4 = SmashCompetitor(
                 id = "4",
                 name = "Justin",
                 tag = "mikkuz",
-                mains = listOf(SmashCharacter.FOX, SmashCharacter.FOX, SmashCharacter.CPTN_FALCON, null))
+                mains = listOf(SmashCharacter.FOX, SmashCharacter.FOX, SmashCharacter.CPTN_FALCON, null)
+        )
     }
 
     @Test
