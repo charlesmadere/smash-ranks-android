6af9e0b81d049b4f2552cb2c056a3172d8522eff
Charles Madere <charlesrmadere@gmail.com>
2018-12-24 18:38:27 -0600

    added save button functionality to SetIdentity and SetRegion toolbars

--- smash-ranks-android/app/src/main/java/com/garpr/android/views/toolbars/SetIdentityToolbar.kt
+++ smash-ranks-android/app/src/main/java/com/garpr/android/views/toolbars/SetIdentityToolbar.kt
@@ -2,7 +2,12 @@ package com.garpr.android.views.toolbars
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import com.garpr.android.R
 import com.garpr.android.extensions.activity
+import com.garpr.android.extensions.layoutInflater
+import kotlinx.android.synthetic.main.gar_toolbar.view.*
+import kotlinx.android.synthetic.main.set_identity_toolbar_items.view.*
 
 class SetIdentityToolbar @JvmOverloads constructor(
         context: Context,
@@ -15,14 +20,34 @@ class SetIdentityToolbar @JvmOverloads constructor(
         fun onSaveClick(v: SetIdentityToolbar)
     }
 
+    init {
+        layoutInflater.inflate(R.layout.set_identity_toolbar_items, menuExpansionContainer)
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        saveButton.setOnClickListener {
+            (activity as? Listeners)?.onSaveClick(this)
+        }
+    }
+
+    override fun onCloseSearchField() {
+        super.onCloseSearchField()
+        saveButton.visibility = View.VISIBLE
+    }
+
+    override fun onOpenSearchField() {
+        super.onOpenSearchField()
+        saveButton.visibility = View.GONE
+    }
+
     override fun refresh() {
         super.refresh()
 
-        if ((activity as? Listeners)?.enableSaveIcon == true) {
-
-        } else {
-
-        }
+        val listeners = activity as? Listeners
+        saveButton.visibility = if (listeners?.showSaveIcon == true) View.VISIBLE else View.GONE
+        saveButton.isEnabled = listeners?.enableSaveIcon == true
     }
 
 }
