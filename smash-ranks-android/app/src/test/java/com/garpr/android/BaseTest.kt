572e97a5df95c2bb5ae2f62ea3763c5882152426
Charles Madere <charlesrmadere@gmail.com>
2019-11-04 21:27:30 -0800

    removed unused annotation stuff from Basetest

--- smash-ranks-android/app/src/test/java/com/garpr/android/BaseTest.kt
+++ smash-ranks-android/app/src/test/java/com/garpr/android/BaseTest.kt
@@ -1,13 +1,10 @@
 package com.garpr.android
 
 import org.junit.Before
-import org.junit.runner.RunWith
 import org.koin.test.AutoCloseKoinTest
-import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
 @Config(application = TestApp::class)
-@RunWith(RobolectricTestRunner::class)
 abstract class BaseTest : AutoCloseKoinTest() {
 
     @Before
