398036885af07486600af6f5c6a58a89f6c3e913
Charles Madere <charlesrmadere@gmail.com>
2019-12-17 19:56:40 -0800

    created new Optional class

---
+++ smash-ranks-android/app/src/main/java/com/garpr/android/data/models/Optional.kt
@@ -0,0 +1,38 @@
+package com.garpr.android.data.models
+
+class Optional<T : Any> private constructor(
+        val item: T?
+) {
+
+    companion object {
+        private val EMPTY = Optional(null)
+
+        fun <T : Any> empty(): Optional<T> {
+            @Suppress("UNCHECKED_CAST")
+            return EMPTY as Optional<T>
+        }
+
+        fun <T : Any> of(item: T): Optional<T> {
+            return Optional(item)
+        }
+
+        fun <T : Any> ofNullable(item: T?): Optional<T> {
+            return if (item == null) {
+                empty()
+            } else {
+                Optional(item)
+            }
+        }
+    }
+
+    val isPresent: Boolean = item != null
+
+    fun orElse(other: T): T {
+        return item ?: other
+    }
+
+    fun require(): T {
+        return checkNotNull(item)
+    }
+
+}
