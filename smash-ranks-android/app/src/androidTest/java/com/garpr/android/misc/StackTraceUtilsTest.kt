8a0b40b2236521bbf89e58ae0ce66d0adb817c85
Charles Madere <charlesrmadere@gmail.com>
2020-01-21 11:03:25 -0800

    added test for StackTraceUtils

---
+++ smash-ranks-android/app/src/androidTest/java/com/garpr/android/misc/StackTraceUtilsTest.kt
@@ -0,0 +1,36 @@
+package com.garpr.android.misc
+
+import com.garpr.android.BaseAndroidTest
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.koin.test.inject
+
+class StackTraceUtilsTest : BaseAndroidTest() {
+
+    protected val stackTraceUtils: StackTraceUtils by inject()
+
+    @Test
+    fun testToStringWithIllegalArgumentException() {
+        val throwable: Throwable = IllegalArgumentException()
+        assertFalse(stackTraceUtils.toString(throwable).isBlank())
+    }
+
+    @Test
+    fun testToStringWithNull() {
+        assertTrue(stackTraceUtils.toString(null).isEmpty())
+    }
+
+    @Test
+    fun testToStringWithRuntimeException() {
+        val throwable: Throwable = RuntimeException()
+        assertFalse(stackTraceUtils.toString(throwable).isBlank())
+    }
+
+    @Test
+    fun testToStringWithThrowable() {
+        val throwable = Throwable()
+        assertFalse(stackTraceUtils.toString(throwable).isBlank())
+    }
+
+}
