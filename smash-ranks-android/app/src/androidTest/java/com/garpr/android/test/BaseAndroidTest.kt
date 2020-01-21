439896bbcbfcd7e6fb65ed9e04b687a973be2f3a
Charles Madere <charlesrmadere@gmail.com>
2020-01-21 15:52:35 -0800

    enable logging for android tests

--- smash-ranks-android/app/src/androidTest/java/com/garpr/android/test/BaseAndroidTest.kt
+++ smash-ranks-android/app/src/androidTest/java/com/garpr/android/test/BaseAndroidTest.kt
@@ -12,13 +12,16 @@ import com.garpr.android.koin.syncModule
 import com.garpr.android.koin.viewModelsModule
 import org.junit.Before
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 import org.koin.test.AutoCloseKoinTest
 
 abstract class BaseAndroidTest : AutoCloseKoinTest() {
 
     private fun initializeKoin() {
         startKoin {
+            androidLogger(Level.DEBUG)
             androidContext(ApplicationProvider.getApplicationContext())
             modules(listOf(androidTestConfigModule, databaseModule, managersModule, miscModule,
                     networkingModule, preferencesModule, repositoriesModule, syncModule,
